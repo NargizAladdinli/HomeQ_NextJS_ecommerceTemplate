8e8bd0affd2d7ec622c1b9da8c98bcd4328e3c3c
nargizaladdinli <nargizalieva05@gmail.com>
2023-07-21 13:04:12 +0400

    module scss

--- homeq/src/pages/_app.js
+++ homeq/src/pages/_app.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/scss/style.scss"
+import "../styles/scss/_style.scss"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
