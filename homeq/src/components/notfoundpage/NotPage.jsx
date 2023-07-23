687011aa9df7bef0df5037985755849d1004f116
nargizaladdinli <nargizalieva05@gmail.com>
2023-07-24 01:29:36 +0400

    module scss use mixin

--- homeq/src/components/notfoundpage/NotPage.jsx
+++ homeq/src/components/notfoundpage/NotPage.jsx
@@ -1,21 +1,24 @@
-import style from "./_notpage.module.scss"
+import Link from "next/link";
+import style from "./_notpage.module.scss";
 
 const NotFoundPage = () => {
-    return(
-        <section className={style.notpage}>
-            <div className="container">
-            <div className={style.notpage404}>
-                <div className={style.imgpage}>
-                    
-                </div>
-                <div>
-                    <h1>404</h1>
-                    <h2>OOPS! PAGE NOT BE FOUND</h2>
-                    <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
-                </div>
-            </div>
-            </div>
-        </section>
-    )
-}
-export default NotFoundPage
+  return (
+    <section className={style.notpage}>
+      <div className="container">
+        <div className={style.notpage404}>
+          <div className={style.imgpage}></div>
+          <div className={style.title}>
+            <h1 className={style.firsttitle}>404</h1>
+            <h2 className={style.secondtitle}>OOPS! PAGE NOT BE FOUND</h2>
+            <p>
+              Sorry but the page you are looking for does not exist, have been
+              removed. name changed or is temporarily unavailable
+            </p>
+        <Link href="/">Back To Home</Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default NotFoundPage;
