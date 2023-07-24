bcb81aaa0078cd6ca78780c2f5b93fca9acb9964
nargizaladdinli <nargizalieva05@gmail.com>
2023-07-25 01:25:11 +0400

    Header page

--- homeq/src/components/header/Header.jsx
+++ homeq/src/components/header/Header.jsx
@@ -1,41 +1,79 @@
-import Link from 'next/link';
-import style from './_header.module.scss'
+import Link from "next/link";
+import style from "./_header.module.scss";
+import { FaShoppingCart, FaAngleDown } from "react-icons/fa";
 
 const Header = () => {
-    return(
-        <>
-        <header>
-            <div className="container">
-                <div>
-                <div className="logo">
-               
-               </div>
-               <nav>
-                   <ul>
-                       <li>
-                           <Link href="/">Home</Link>
-                       </li>
-                       <li>
-                           <Link href="#">About</Link>
-                       </li>
-                       <li>
-                           <Link href="#">Shop</Link>
-                       </li>
-                       <li>
-                           <Link href="#">News</Link>
-                       </li>
-                       <li>
-                           <Link href="#">Pages</Link>
-                       </li>
-                       <li>
-                           <Link href="#">Contact</Link>
-                       </li>
-                   </ul>
-               </nav>
-                </div>
+  return (
+    <>
+      <header className={style.header}>
+        <div className="container">
+          <div className={style.headerflex}>
+            <div>
+              <Link className={style.logolink} href="#">
+                Home<span>Q</span>
+              </Link>
             </div>
-        </header>
-        </>
-    )
-}
+            <nav>
+              <ul className={style.listnav}>
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                <li>
+                  <Link href="#">About</Link>
+                </li>
+                <li>
+                  <Link href="#">Shop</Link>
+                </li>
+                <li>
+                  <Link href="#">News</Link>
+                </li>
+                <li className={style.linav}>
+                  <Link href="#">
+                    Pages <FaAngleDown />
+                  </Link>
+                  <ul className={style.dropdown}>
+                    <li>
+                        <Link className={style.listdrop} href="#">Portofolio</Link>
+                    </li>
+                    <li>
+                        <Link className={style.listdrop} href="#">Our Agent</Link>
+                    </li>
+                    <li>
+                        <Link className={style.listdrop} href="#">Our Service</Link>
+                    </li>
+                    <li>
+                        <Link className={style.listdrop} href="#">Wishlist</Link>
+                    </li>
+                    <li>
+                        <Link className={style.listdrop} href="#">Location</Link>
+                    </li>
+                    <li>
+                        <Link className={style.listdrop} href="#">FAQ</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <Link href="#">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+            <div className={style.shoping}>
+              <div>
+                <Link href="#">
+                  <FaShoppingCart />
+                </Link>
+              </div>
+              <div>
+                <Link href="#">Sign in</Link>
+              </div>
+              <div>
+                <Link href="#">Register</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 export default Header;
--- homeq/src/pages/_app.js
+++ homeq/src/pages/_app.js
@@ -1,6 +1,6 @@
+import "@/styles/scss/_style.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "@/components/layout/Layout";
-import "@/styles/scss/_style.scss";
 
 export default function App({ Component, pageProps }) {
   return(
