7af0cd873e3d349cb01924ca9ecc327fb07e2156
nargizaladdinli <nargizalieva05@gmail.com>
2023-07-24 17:30:49 +0400

    Header nav menu

--- homeq/src/components/header/Header.jsx
+++ homeq/src/components/header/Header.jsx
@@ -1,10 +1,40 @@
+import Link from 'next/link';
 import style from './_header.module.scss'
 
 const Header = () => {
     return(
-
         <>
-        <h1 className={style.title}>salam</h1>
+        <header>
+            <div className="container">
+                <div>
+                <div className="logo">
+               
+               </div>
+               <nav>
+                   <ul>
+                       <li>
+                           <Link href="/">Home</Link>
+                       </li>
+                       <li>
+                           <Link href="#">About</Link>
+                       </li>
+                       <li>
+                           <Link href="#">Shop</Link>
+                       </li>
+                       <li>
+                           <Link href="#">News</Link>
+                       </li>
+                       <li>
+                           <Link href="#">Pages</Link>
+                       </li>
+                       <li>
+                           <Link href="#">Contact</Link>
+                       </li>
+                   </ul>
+               </nav>
+                </div>
+            </div>
+        </header>
         </>
     )
 }
