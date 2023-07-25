4293106a5a7856e93d24a75ae78ebeac60fa3ad0
nargizaladdinli <nargizalieva05@gmail.com>
2023-07-25 23:03:08 +0400

    explore page and style

--- homeq/src/components/explore/Explore.jsx
+++ homeq/src/components/explore/Explore.jsx
@@ -3,21 +3,21 @@ import style from "./_explore.module.scss";
 
 const Explore = () => {
   return (
-    <section>
-      <div style={{ maxWidth: "1140px" }} className="container">
+    <section className={style.explore}>
+      <div style={{maxWidth:"1140px"}} className="container">
         <div className={style.ground}>
           <div className="row align-items-center">
             <div className="col-lg-3">
               <img src="" alt="" />
             </div>
             <div className="col-lg-6">
-              <div>
-                <h3>Looking for a Dream Home?</h3>
-                <p>We can help you relize your dream of a new home</p>
+              <div className={style.title}>
+                <h3 className={style.h3title}>Looking for a Dream Home?</h3>
+                <p className={style.ptitle}>We can help you relize your dream of a new home</p>
               </div>
             </div>
             <div className="col-lg-3">
-                <Link href="#">Explore Properties</Link>
+                <Link className={style.explorelink} href="#">Explore Properties</Link>
             </div>
           </div>
         </div>
