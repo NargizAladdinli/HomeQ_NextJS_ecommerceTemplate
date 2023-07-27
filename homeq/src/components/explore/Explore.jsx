f7bbb711b95b83959d4007aa3b1f48145061cfc2
nargizaladdinli <nargizalieva05@gmail.com>
2023-07-27 16:49:02 +0400

    Footer Page

--- homeq/src/components/explore/Explore.jsx
+++ homeq/src/components/explore/Explore.jsx
@@ -3,26 +3,46 @@ import style from "./_explore.module.scss";
 
 const Explore = () => {
   return (
+    <>
     <section className={style.explore}>
-      <div style={{maxWidth:"1140px"}} className="container">
+      <div style={{ maxWidth: "1140px" }} className="container">
         <div className={style.ground}>
           <div className="row align-items-center">
             <div className="col-lg-3">
-              <img src="" alt="" />
+              
             </div>
             <div className="col-lg-6">
               <div className={style.title}>
                 <h3 className={style.h3title}>Looking for a Dream Home?</h3>
-                <p className={style.ptitle}>We can help you relize your dream of a new home</p>
+                <svg
+                  width="209"
+                  height="12"
+                  viewBox="0 0 209 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 10.9999C1 10.9999 61.4596 2.16784 100.541 1.08487C142.325 -0.0730059 208 11 208 11"
+                    stroke="#DE8556"
+                    stroke-width="2"
+                  />
+                </svg>
+
+                <p className={style.ptitle}>
+                  We can help you relize your dream of a new home
+                </p>
               </div>
             </div>
             <div className="col-lg-3">
-                <Link className={style.explorelink} href="#">Explore Properties</Link>
+              <Link className={style.explorelink} href="#">
+                Explore Properties
+              </Link>
             </div>
           </div>
         </div>
       </div>
     </section>
+    </>
   );
 };
 export default Explore;
--- homeq/src/components/footer/Footer.jsx
+++ homeq/src/components/footer/Footer.jsx
@@ -1,16 +1,63 @@
 import Explore from "../explore/Explore";
 import style from "./_footer.module.scss";
+import Link from "next/link";
+import { FaPhone, FaEnvelope, FaSearchLocation } from "react-icons/fa";
 
 const Footer = () => {
-    return(
-        <>
-        <Explore/>
-        <footer>
-            <div style={{maxWidth:"1140px"}} className="container">
-                
+  return (
+    <>
+      <Explore />
+      <footer className={style.footer}> 
+        <div style={{ maxWidth: "1140px" }} className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div>
+                <Link className={style.logolink} href="#">
+                  Home<span>Q</span>
+                </Link>
+                <p className={style.pfooter}>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. </p>
+              </div>
+              <div className={style.adress}>
+                <ul className={style.list}> 
+                    <li>
+                        <Link className={style.icon} href="#"><FaPhone/></Link>
+                        <Link className={style.title} href="#">
+                            <p>Phone</p>
+                            <span>(208) 555-0112</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className={style.icon} href="#"><FaEnvelope/></Link>
+                        <Link className={style.title} href="mailto:homeq.example@gmail.com">
+                            <p>Email</p>
+                            <span>homeq.example@gmail.com</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className={style.icon} href="#"><FaSearchLocation/></Link>
+                        <Link className={style.title} href="#">
+                            <p>Address</p>
+                            <span>3517 W. Gray St. Utica, Pennsylvania 57867</span>
+                        </Link>
+                    </li>
+                </ul>
+              </div>
             </div>
-        </footer>
-        </>
-    )
-}
+            <div className="col-lg-2">
+                <p>Company</p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div className="col-lg-2"></div>
+            <div className="col-lg-4"></div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 export default Footer;
