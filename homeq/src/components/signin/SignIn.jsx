61bc36960b44d05b532495d0ba04cff25bb3ac69
nargizaladdinli <nargizalieva05@gmail.com>
2023-07-28 14:54:05 +0400

    Sign In

--- homeq/src/components/signin/SignIn.jsx
+++ homeq/src/components/signin/SignIn.jsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
 import style from "./_signin.module.scss";
 
 const SignIn = () => {
+    const signIn = (e) => {
+        e.preventDefault()
+    }
     return(
         <section className={style.sign}>
             <div className="container">
@@ -8,13 +12,16 @@ const SignIn = () => {
                     <h2>Sign In to your account</h2>
                     <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. </p>
                 </div>
-                <form className={style.form}>
+                <form onSubmit={signIn} className={style.form}>
                     <div className={style.inputdiv}>
                     <input type="email" name="email" placeholder="Email or Username" />
                     <input type="password" name="password" placeholder="Password"/>
-                    </div>
-                    <div>
                     <span>Forgot Password?</span>
+                    <button type="submit">Login</button>
+                    <div className={style.create}>
+                        <span>Dont have an account? </span>
+                        <Link href="#"> Create Account</Link>
+                    </div>
                     </div>
                 </form>
             </div>
